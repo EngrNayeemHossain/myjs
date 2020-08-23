97b7241ad557c6a1c03735dba1e63dab176b652c
Ayesha Siddika <ayesha56sid@gmail.com>
2020-08-23 18:46:13 +0600

    null vs undefined

---
+++ 24.3.js
@@ -0,0 +1,22 @@
+//null vs undefined
+let best;
+console.log(best);
+
+function add(num1,num2){
+    console.log(num1+num2);
+}
+console.log(add(13,12));
+
+function adds(num3,num4){
+    return num3+num4;
+}
+console.log(adds(13,12));
+
+const lover={name:"Javed",phone:01717};
+console.log(lover.name);
+
+let fun=undefined;
+console.log(fun);
+
+let ages=[14,15,16];
+console.log(ages[11]);
