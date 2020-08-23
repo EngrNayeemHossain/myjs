a6ab21c953021003e90a60026b5983b3e027a13d
Ayesha Siddika <ayesha56sid@gmail.com>
2020-08-23 18:17:46 +0600

    project done

---
+++ 24.2.js
@@ -0,0 +1,54 @@
+const age=4;
+if(age>2){
+    console.log("Condition is true");
+}
+else{
+    console.log("Condition is not true.");
+}
+
+const names="";
+if(names){
+    console.log("Condition is true");
+}
+else{
+    console.log("Condition is not true.");
+}
+
+const name=" ";
+if(name){
+    console.log("Condition is true");
+}
+else{
+    console.log("Condition is not true.");
+}
+
+let named=false;
+if(named){
+    console.log("Condition is true");
+}
+else{
+    console.log("Condition is not true.");
+}
+
+let a=NaN;
+if(a){
+    console.log("Condition is true");
+}
+else{
+    console.log("Condition is not true.");
+}
+
+let b=0;
+if(b || b==0){
+    console.log("Condition is true");
+}
+else{
+    console.log("Condition is not true.");
+}
+let c=12;
+if(c || c==0){
+    console.log("Condition is true");
+}
+else{
+    console.log("Condition is not true.");
+}
